c1220583a9bb10fb50bf4b8d9b7b432ff6046c9e
villivald <maxim.villivald@gmail.com>
2023-01-30 18:26:30 +0200

    update dependencies

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -1,7 +1,7 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 export default async function AuthStatus() {
-  const session = await unstable_getServerSession();
+  const session = await getServerSession();
 
   return <div>{session && <p>Signed in as {session.user?.email}</p>}</div>;
 }
